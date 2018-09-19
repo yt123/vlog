1cf9b0e5e617d09a79b47b4452bf1a52b53a2b33
Apple <yina727@gmail.com>
2018-09-19 23:04:31 +0200

    first commit

---
+++ sentiment.R
@@ -0,0 +1,136 @@
+# Make sure to change this
+setwd("/Users/Apple/Desktop/big data/youtube-personality")
+library(tidyverse)
+library(tidytext)
+library(olsrr)
+library(stringr)
+
+" 
+Read in the CSV file
+"
+personality = as.tibble(read.csv("YouTube-Personality-Personality_impression_scores_train.csv", 
+                        encoding = "UTF-8",
+                        sep = " "))
+
+gender = as.tibble(read.csv("YouTube-Personality-gender.csv",
+                            encoding = "UTF-8",
+                            sep = " "))
+
+audiovisual = as.tibble(read.csv("YouTube-Personality-audiovisual_features.csv",
+                                 encoding = "UTF-8",
+                                 sep = " "))
+
+transcripts = as.tibble(read.csv("transcripts.csv",
+                                 encoding = "UTF-8",
+                                 sep = ","))
+
+transcripts$index = as.character(transcripts$index)
+
+transcripts =
+  transcripts %>%
+  rename(vlogId = index,
+         transcript = X0) %>%
+  mutate(vlogId = substr(vlogId, 1, nchar(vlogId)-4))
+
+personality$vlogId = as.character(personality$vlogId)
+gender$vlogId = as.character(gender$vlogId)
+audiovisual$vlogId = as.character(audiovisual$vlogId)
+
+training_data = 
+  personality %>%
+  left_join(gender, on = c("vlogID" = "vlogID")) %>%
+  left_join(audiovisual, on = c("vlogID" = "vlogID")) %>%
+  left_join(transcripts, on = c("clogID" = "vlogID"))
+
+#tokenization
+training_data$transcript <- as.vector(training_data$transcript)
+tidy_data <- training_data %>% 
+  unnest_tokens(word, transcript, to_lower = TRUE) %>% 
+  anti_join(get_stopwords())
+
+#count the total number of words for each person
+total_words <- tidy_data %>% 
+  group_by(vlogId) %>% 
+  count() %>% 
+  rename(total_words = n)
+
+#count the percentage of words related to big 5
+words_E = c("bar","drinks", "restaurant", "dancing", "restaurants", "grandfather", "miami", "countless", "drinking", "shots", "girls", "glorious", "pool", "crowd", "sang", "grilled")
+words_A = c("wonderful", "together", "visiting", "morning", "spring", "walked", "beautiful", "staying", "felt", "share", "gray", "joy", "afternoon", "day", "moments", "hug", "glad")
+words_C = c("completed", "adventure", "adventures", "enjoying", "hawaii", "it's", "deck")
+words_N = c("awful", "though", "lazy", "worse", "depressing", "irony", "terrible", "stressful", "horrible", "sort", "annoying", "ashamed", "ban")
+words_O = c("folk", "humans", "of", "poet", "art", "by", "universe", "poetry", "narrative", "culture", "century", "sexual", "films", "novel", "decades", "ink", "passage", "literature", "blues")
+words_nE = c("other", "cats", "computer", "minor")
+words_nA = c("porn", "cost", "fuck")
+words_nC = c("stupid", "boring", "desperate", "saying", "utter", "it's", "extreme")
+words_nN = c("road", "southern", "visited", "ground", "oldest", "invited", "completed")
+words_nO = c("giveaway")
+
+check_words <- function(wordlist){
+  tidy_data %>% 
+    filter(word %in% wordlist) %>% 
+    group_by(vlogId) %>% 
+    count() %>% 
+    left_join(total_words) %>% 
+    mutate(n/total_words) %>% 
+    select(-n, -total_words)
+}
+
+E_words <- check_words(words_E) %>% 
+  rename(words_Extraversion = 'n/total_words')
+
+A_words <- check_words(words_A) %>% 
+  rename(words_Agreeableness = 'n/total_words')
+
+C_words <- check_words(words_C) %>% 
+  rename(words_Conscientiousness = 'n/total_words')
+
+N_words <- check_words(words_N) %>% 
+  rename(words_Neuroticism = 'n/total_words')
+
+O_words <- check_words(words_O) %>% 
+  rename(words_Openness = 'n/total_words')
+
+nE_words <- check_words(words_nE) %>% 
+  rename(words_nExtraversion = 'n/total_words')
+
+nA_words <- check_words(words_nA) %>% 
+  rename(words_nAgreeableness = 'n/total_words')
+
+nC_words <- check_words(words_nC) %>% 
+  rename(words_nConscientiousness = 'n/total_words')
+
+nN_words <- check_words(words_nN) %>% 
+  rename(words_nNeuroticism = 'n/total_words')
+
+nO_words <- check_words(words_nO) %>% 
+  rename(words_nOpenness = 'n/total_words')
+
+#putting them bag to the training data
+training_data <- training_data %>% 
+  left_join(E_words) %>%
+  left_join(A_words) %>%
+  left_join(C_words) %>%
+  left_join(N_words) %>%
+  left_join(O_words) %>%
+  left_join(nE_words) %>%
+  left_join(nA_words) %>%
+  left_join(nC_words) %>%
+  left_join(nN_words) %>%
+  left_join(nO_words) %>% 
+  replace(., is.na(.), 0)
+
+#sentiment analysis with afinn
+afinn <- tidy_data %>% 
+  inner_join(get_sentiments('afinn')) %>% 
+  rename(sentiment = score)
+
+#sentiment analysis with nrc
+nrc <- tidy_data %>% 
+  inner_join(get_sentiments('nrc')) %>%
+  count(`vlogId`, sentiment) %>%
+  spread(sentiment, n, fill = 0)
+
+training_data <- training_data %>% 
+  left_join(nrc) %>% 
+  select(-transcript)
